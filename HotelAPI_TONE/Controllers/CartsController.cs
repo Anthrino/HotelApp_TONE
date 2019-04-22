92681f2515ecc0cea68f1027aaa9a356ea67c004
Anthrino <jerinjohn101@gmail.com>
2019-04-22 22:06:53 +0530

    Add Cart model, API controller

---
+++ HotelAPI_TONE/Controllers/CartsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelAPI_TONE.Models;
+
+namespace HotelAPI_TONE.Controllers
+{
+    [Route("api/carts")]
+    [ApiController]
+    public class CartsController : ControllerBase
+    {
+        private readonly HotelAPI_TONEContext _context;
+
+        public CartsController(HotelAPI_TONEContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Carts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cart>>> GetCart()
+        {
+            return await _context.Cart.ToListAsync();
+        }
+
+        // GET: api/Carts/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCart([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cart);
+        }
+
+        // PUT: api/Carts/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCart([FromRoute] int id, [FromBody] Cart cart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cart.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cart).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CartExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Carts
+        [HttpPost]
+        public async Task<IActionResult> PostCart([FromBody] Cart cart)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Cart.Add(cart);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCart", new { id = cart.Id }, cart);
+        }
+
+        // DELETE: api/Carts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCart([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            _context.Cart.Remove(cart);
+            await _context.SaveChangesAsync();
+
+            return Ok(cart);
+        }
+
+        private bool CartExists(int id)
+        {
+            return _context.Cart.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HotelAPI_TONE/Models/Cart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAPI_TONE.Models
+{
+	public class Cart
+	{
+		public int Id { get; set; }
+		public int userId { get; set; }
+		public int itemId { get; set; }
+	}
+}
