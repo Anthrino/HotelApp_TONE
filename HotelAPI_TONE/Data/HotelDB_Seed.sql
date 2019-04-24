416f451790df1fb3c3cd7bf59d466c6f7223e57c
Anthrino <jerinjohn101@gmail.com>
2019-04-24 19:42:57 +0530

    Add DB init seed scripts

---
+++ HotelAPI_TONE/Data/HotelDB_Seed.sql
@@ -0,0 +1,20 @@
+﻿use HotelDB;
+GO
+insert into item values ("Pizza", "Snacks", "Italian", 5);
+GO
+insert into item values ("Brownie", "Desert", "Sweet", 1.5);
+GO 
+insert into item values ("Beer", "Beverage", "Casual drink", 1);
+GO
+insert into users values("admin@hotel.com", "root", 1);
+GO
+insert into users values("filip@hotel.com", "qwerty", 3);
+GO
+insert into users values("jones@hotel.com", "b99", 3);
+GO
+insert into cart values(2, 1);
+GO
+insert into cart values(2, 3);
+GO
+insert into cart values(3, 2);
+GO
