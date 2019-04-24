8fe5ff6ad6d89362c42906249342b8b9e2663e97
Anthrino <jerinjohn101@gmail.com>
2019-04-24 19:43:30 +0530

    Add DBContext

---
+++ HotelAPI_TONE/Data/HotelAPI_TONEContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HotelAPI_TONE.Models;
+
+namespace HotelAPI_TONE.Models
+{
+    public class HotelAPI_TONEContext : DbContext
+    {
+        public HotelAPI_TONEContext (DbContextOptions<HotelAPI_TONEContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HotelAPI_TONE.Models.Item> Item { get; set; }
+
+        public DbSet<HotelAPI_TONE.Models.Cart> Cart { get; set; }
+
+        public DbSet<HotelAPI_TONE.Models.Users> Users { get; set; }
+    }
+}
