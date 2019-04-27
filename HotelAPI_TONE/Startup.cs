fa364a2e959bbc23f63079955faf9c4d95f49fb2
Anthrino <jerinjohn101@gmail.com>
2019-04-27 22:49:45 +0530

    Proj file updates

--- HotelAPI_TONE/Startup.cs
+++ HotelAPI_TONE/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
+using HotelAPI_TONE.Models;
 
 namespace HotelAPI_TONE
 {
@@ -26,6 +28,9 @@ namespace HotelAPI_TONE
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+		    services.AddDbContext<HotelAPI_TONEContext>(options =>
+		            options.UseSqlServer(Configuration.GetConnectionString("HotelAPI_TONEContext")));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
