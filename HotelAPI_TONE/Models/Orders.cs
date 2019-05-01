5dd336fcde44d17742f9c14f7fb58985b80f1d00
Anthrino <jerinjohn101@gmail.com>
2019-05-01 18:55:16 +0530

    Add Orders API impl

---
+++ HotelAPI_TONE/Models/Orders.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelAPI_TONE.Models
+{
+	public class Orders
+	{
+		public int Id { get; set; }
+		public int userId { get; set; }
+		public int itemId { get; set; }
+		public decimal price { get; set; }
+		public int quantity { get; set; }
+		public DateTime dop { get; set; }
+	}
+}
