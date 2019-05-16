316f17de26fb737aca67a3e75345156b103d40ce
Anthrino <jerinjohn101@gmail.com>
2019-05-16 22:42:22 +0530

    Add Shared proj - Models

---
+++ Shared/Models/Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Item
+	{
+		public int Id { get; set; }
+		[DisplayName("Item")]
+		[Required(ErrorMessage = "Mandatory Field")] 
+		public string Title { get; set; }
+		[DisplayName("Category")]
+		[Required(ErrorMessage = "Mandatory Field")] 
+		public string Category { get; set; }
+		[DisplayName("Item Description")]
+		[Required(ErrorMessage = "Mandatory Field")] 
+		public string Description { get; set; }
+		//public byte[] image { get; set; }
+		[DataType(DataType.Currency)]
+		[Required(ErrorMessage = "Mandatory Field")]
+		public decimal Price { get; set; }
+	}
+}
---
+++ Shared/Models/Users.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class Users
+	{
+		public int Id { get; set; }
+		public string displayName { get; set; }
+		public string username { get; set; }
+		public int orderCount { get; set; }
+		public string token { get; set; }
+		public int role {
+			get
+			{
+				return (int)this.roleType;
+			}
+			set
+			{
+				roleType = (roleTypes)value;
+			}
+		}
+		[EnumDataType(typeof(roleTypes))]
+		public roleTypes roleType { get; set; }
+		
+	}
+
+	public enum roleTypes
+	{
+		Admin = 1,
+		Vendor = 2,
+		Customer = 3
+	}
+}
+
+  
