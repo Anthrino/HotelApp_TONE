6fd02cbcde97972f522aa009e84e68b52341830c
Anthrino <jerinjohn101@gmail.com>
2019-05-18 17:44:47 +0530

    Add Sendgrid Email Notifier Service - Orders

--- BusinessLogic/Processors/OrderProcessor.cs
+++ BusinessLogic/Processors/OrderProcessor.cs
@@ -1,12 +1,9 @@
 ﻿using DataAccessLayer.Repository;
-using Microsoft.EntityFrameworkCore;
+using Shared.Helpers;
 using Shared.Models;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Net;
-using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace BusinessLogic.Processors
@@ -51,7 +48,7 @@ namespace BusinessLogic.Processors
 			return orderItems;
 		}
 
-		public async Task<bool> AddOrder(int userId)
+		public async Task<bool> AddOrder(int userId, string apiKey)
 		{
 			IEnumerable<Cart> cartItems = await cartProcessor.GetCart(userId);
 
@@ -84,74 +81,10 @@ namespace BusinessLogic.Processors
 			//Generate orderItems list for email notification
 			List<OrderItem> orderItems = GetOrderItems(orderId).Result.ToList();
 
-			//TODO: EMAIL
-			//OrderRepository.EmailNotifier("jerinjohn101@outlook.com", orderItems).ToString());
-			//static bool mailSent = false;
-			//private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
-			//{
-			//	// Get the unique identifier for this asynchronous operation.
-			//	String token = (string)e.UserState;
+			Users user = await userProcessor.GetUser(userId);
 
-			//	if (e.Cancelled)
-			//	{
-			//		Console.WriteLine("[{0}] Send canceled.", token);
-			//	}
-			//	if (e.Error != null)
-			//	{
-			//		Console.WriteLine("[{0}] {1}", token, e.Error.ToString());
-			//	}
-			//	else
-			//	{
-			//		Console.WriteLine("Message sent.");
-			//	}
-			//	mailSent = true;
-			//}
-
-			//public static bool EmailNotifier(string usermail, IEnumerable<OrderItem> orders)
-			//{
-			//	// Command-line argument must be the SMTP host.
-			//	SmtpClient client = new SmtpClient();
-			//	{
-			//		client.Host = "smtp.outlook.com";
-			//		client.Port = 587;
-			//		client.EnableSsl = true;
-
-			//		client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
-			//		client.Credentials = new NetworkCredential("jerinjohn101@outlook.com", "");
-			//		client.UseDefaultCredentials = false;
-			//		client.Timeout = 600000;
-			//	}
-
-			//	MailAddress from = new MailAddress("jerinjohn101@outlook.com", "MVC Hotel " + (char)0xD8 + " Order", System.Text.Encoding.UTF8);
-			//	MailAddress to = new MailAddress(usermail);
-
-			//	MailMessage message = new MailMessage(from, to);
-			//	message.Body = "Automated order notification from Hotel.com";
-
-			//	string someArrows = new string(new char[] { '\u2190', '\u2191', '\u2192', '\u2193' });
-			//	message.Body += Environment.NewLine + someArrows;
-			//	message.BodyEncoding = System.Text.Encoding.UTF8;
-
-			//	message.Body += "<table class=\"table\"> < thead > < tr > < th > Order Id# </ th > < th > Title </ th > < th > Price </ th > < th > Quantity </ th > < th ></ th > </ tr > </ thead > < tbody >";
-			//	foreach (var item in orders)
-			//	{
-			//		message.Body += " <tr > < td >" + item.order.orderId + "</ td > < td > " + item.item.Title + " </ td > < td >" + item.item.Price + "</ td > < td >" + item.order.quantity + " </ td ></tr>";
-			//	}
-			//	message.Body += "</tbody></table>";
-
-			//	message.Subject = "Order Confirmation - Hotel.com" + someArrows;
-			//	message.SubjectEncoding = System.Text.Encoding.UTF8;
-
-			//	string userState = "test message1";
-			//	client.SendAsync(message, userState);
-
-			//	client.SendCompleted += new SendCompletedEventHandler(SendCompletedCallback);
-
-			//	message.Dispose();
-
-			//	return mailSent;
-			//}
-			return true;
+			//Call Sendgrid Email Notification Service
+			return await new EmailService().EmailNotifier(apiKey, user, orderItems);
 		}
 	}
 }
---
+++ Shared/Helpers/EmailService.cs
@@ -0,0 +1,42 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using Shared.Models;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Shared.Helpers
+{
+	public class EmailService
+	{
+		public async Task<bool> EmailNotifier(string apiKey, Users user, IEnumerable<OrderItem> orders)
+		{
+			var client = new SendGridClient(apiKey);
+			var from = new EmailAddress("hoteltone@outlook.com", "Hotel Admin");
+			List<EmailAddress> tos = new List<EmailAddress>
+			{
+			  new EmailAddress(user.username, user.displayName),
+			};
+
+			var subject = "TONE Hotel - Order Confirmation";
+			var htmlContent = await System.IO.File.ReadAllTextAsync("Shared/Templates/orderConfMailTemplate.html");
+
+			var body = htmlContent.Split("<tbody>")[1].Split("</tbody>")[0];
+			string orderList = "";
+
+			foreach (var item in orders)
+			{
+				orderList += body.Replace("orderId", item.order.orderId.ToString()).Replace("Title", item.item.Title.ToString()).Replace("Price", item.item.Price.ToString()).Replace("quantity", item.order.quantity.ToString());
+			}
+
+			htmlContent = htmlContent.Replace(body, orderList);
+
+			var displayRecipients = false; // set this to true if you want recipients to see each others mail id 
+			var msg = MailHelper.CreateSingleEmailToMultipleRecipients(from, tos, subject, "", htmlContent, false);
+			var response = await client.SendEmailAsync(msg);
+			if (response.StatusCode == HttpStatusCode.OK)
+				return true;
+			return false;
+		}
+	}
+}
